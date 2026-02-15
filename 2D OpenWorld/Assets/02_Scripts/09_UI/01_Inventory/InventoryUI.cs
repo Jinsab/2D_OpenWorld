9cf22d854952ab365db295dc26e66acfc209b2d4
Jinwoo Kim <36787932+Jinsab@users.noreply.github.com>
2026-02-15 23:04:15 +0900

    인벤토리 시스템 2
    인벤토리 UI 기능 추가 예정
    1. 데이터 로직
    2. 전체 UI 관리
    3. 슬롯 단위 UI
    4. 마우스 드래그
    
    5. 쓰레기통 UI 추가 필요
    6. 추후 장비 아이템 UI
    7. 제작 시스템과 연계 예정

---
+++ 2D OpenWorld/Assets/02_Scripts/09_UI/01_Inventory/InventoryUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+/*  
+ *  [프로젝트 제목]
+ *  2D 오픈월드 생존제작
+ *             
+ *  [프로젝트 일자]
+ *  파일 생성 일자 : 26.02.15 오후 21:06
+ *  마지막 수정 일자 : 26.02.15 오후 21:06
+ *  
+ *  [스크립트 목적 및 내용]
+ *  1. 인벤토리 시스템 - 인벤토리 UI 관리
+ *    1-1. UI에서 slots 리스트를 직접 수정하면 안됨(반드시 Inventory 함수를 통해 처리)
+ *    
+ *  2. 큰 그림
+ *    - Inventory System
+ *      ├─ Inventory (인벤토리 데이터 로직)
+ *      └─ Inventory UI (전체 UI 관리)
+ *         ├─ InventorySlotUI (슬롯 단위 UI)
+ *         └─ DragController (마우스 드래그 전담)
+ *      
+ *  [스크립트 작성 도움 출처]
+ *  1. 
+ */
+
+public class InventoryUI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
