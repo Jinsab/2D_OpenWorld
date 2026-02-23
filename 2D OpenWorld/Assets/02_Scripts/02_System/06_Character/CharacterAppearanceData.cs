d09c61f5b36b3d945fda68134bce76705c553946
Jinwoo Kim <36787932+Jinsab@users.noreply.github.com>
2026-02-23 15:19:31 +0900

    캐릭터 커스텀 마이징 기능 추가 예정

---
+++ 2D OpenWorld/Assets/02_Scripts/02_System/06_Character/CharacterAppearanceData.cs
@@ -0,0 +1,10 @@
+using UnityEngine.U2D.Animation;
+
+[System.Serializable]
+public class CharacterAppearanceData
+{
+    public SpriteLibraryAsset bodyAsset; // 피부색
+    public SpriteLibraryAsset headAsset; // 얼굴/눈 모양
+    public SpriteLibraryAsset hairAsset; // 헤어 스타일
+    public SpriteLibraryAsset clothAsset; // 기본 옷
+}
