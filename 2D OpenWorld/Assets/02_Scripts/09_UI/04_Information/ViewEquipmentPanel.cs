3277c534070974d9a095b190d9e28013b5b7911b
Jinwoo Kim <36787932+Jinsab@users.noreply.github.com>
2026-03-20 01:27:39 +0900

    [UI] 정보창 선택 기능 연결
    1. Equipment window
    2. Skill Window
    3. Compendium Window
    
    3개의 Window의 창이 서로 상호작용하며 작동하게 됨

---
+++ 2D OpenWorld/Assets/02_Scripts/09_UI/04_Information/ViewEquipmentPanel.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+/*  
+ *  [프로젝트 제목]
+ *  2D 오픈월드 생존제작
+ *             
+ *  [프로젝트 일자]
+ *  파일 생성 일자 : 26.03.20 오전 01:02
+ *  마지막 수정 일자 : 26.03.20 오전 01:02
+ *  
+ *  [스크립트 목적 및 내용]
+ *  1. 인벤토리 시스템 - 슬롯 단위 인벤토리
+ *    1-1. 플레이어, 창고, 화로(용광로), 그 외 작업대 등에 입혀 사용할 수 있음
+ *      
+ *  [스크립트 작성 도움 출처]
+ *  1. 
+ */
+
+public class ViewEquipmentPanel : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
