8ad839e9afe4177df6ef40fe2fcdafeb5fb6f712
Jinwoo Kim <36787932+Jinsab@users.noreply.github.com>
2026-03-23 22:11:48 +0900

    추후 작업 예정 정리
    1. 마우스 커서 - 커스텀 커서 고려하기
    2. 인벤토리에서 아이템 선택 시 위치 수정하기 (현재는 정 가운데)

---
+++ 2D OpenWorld/Assets/02_Scripts/09_UI/CustomCursor.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+/*  
+ *  [프로젝트 제목]
+ *  2D 오픈월드 생존제작
+ *             
+ *  [프로젝트 일자]
+ *  파일 생성 일자 : 26.03.23 오후 21:45
+ *  마지막 수정 일자 : 26.03.23 오후 21:45
+ *  
+ *  [스크립트 목적 및 내용]
+ *  1. 커스텀 커서 스크립트
+ *    1-1. 
+ *     
+ *  [스크립트 작성 도움 출처]
+ *  1.
+ */
+
+public class CustomCursor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
