e0db9a06b307526c79769b152f3cdf0b4ce8c364
Jinwoo Kim <36787932+Jinsab@users.noreply.github.com>
2026-04-05 18:13:37 +0900

    장비 교체 로직

--- 2D OpenWorld/Assets/02_Scripts/03_Player/02_Equipment/PlayerEquipment.cs
+++ 2D OpenWorld/Assets/02_Scripts/03_Player/02_Equipment/PlayerEquipment.cs
@@ -1,6 +1,7 @@
-using UnityEngine;
-using System.Collections.Generic;
 using AYellowpaper.SerializedCollections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 /*  
  *  [프로젝트 제목]
@@ -8,7 +9,7 @@ using AYellowpaper.SerializedCollections;
  *             
  *  [프로젝트 일자]
  *  파일 생성 일자 : 26.02.20 오전 01:37
- *  마지막 수정 일자 : 26.03.30 오전 04:53
+ *  마지막 수정 일자 : 26.04.05 오후 17:19
  *  
  *  [스크립트 목적 및 내용]
  *  1. 플레이어 장비 시스템
@@ -27,9 +28,8 @@ public class PlayerEquipment : MonoBehaviour
 {
     // 각 슬롯별 현재 장착된 아이템 저장
     // private Dictionary<EquipmentSlot, EquipmentItem> currentEquipments = new Dictionary<EquipmentSlot, EquipmentItem>();
-    [SerializeField]
-    private SerializedDictionary<EquipmentSlot, EquipmentItem> currentEquipments = new();
-    private PlayerStats stats;
+    [SerializeField] private EquipmentInventory equipmentInv;
+    [SerializeField] private SerializedDictionary<EquipmentSlot, List<EquipmentInventorySlot>> currentEquipments = new(); private PlayerStats stats;
     private CharacterVisuals visuals;
     private WeaponController weaponController;
 
@@ -40,16 +40,84 @@ public class PlayerEquipment : MonoBehaviour
         weaponController = GetComponent<WeaponController>();
     }
 
-    public void Equip(EquipmentItem newItem)
+    private void OnEnable()
     {
+        // 이벤트 구독
+        equipmentInv.OnEquipmentChanged += UpdatePlayerState;
+    }
+
+    private void OnDisable()
+    {
+        // 구독 해제 (메모리 누수 방지)
+        equipmentInv.OnEquipmentChanged -= UpdatePlayerState;
+    }
+
+    // EquipmentInventory에서 InitDictionary 호출 시, 해당 함수 호출
+    public void CopyDictionary(SerializedDictionary<EquipmentSlot, List<EquipmentInventorySlot>> dict)
+    {
+        // 1. Dictionary 초기화 (부위별 슬롯 개수 설정)
+        currentEquipments.Clear();
+        currentEquipments = dict;
+    }
+
+
+    private void UpdatePlayerState(EquipmentSlot slot, int index)
+    {
+        // 1. 능력치 초기화 및 재계산
+        Equip(slot, index);
+        //UpdateStats();
+
+        //// 2. 외형 업데이트
+        //UpdateAppearance();
+
+        //// 3. 무기 컨트롤러 업데이트
+        //UpdateWeapon();
+
+        //// 4. 세트 효과 체크 (이미 구현된 시스템 호출)
+        //SetEffectManager.Instance.CheckSetEffects(equipmentInv.GetAllEquippedItems());
+    }
+
+    public void UpdateStats() // EquipmentItem newItem
+    {
+        //// 1. 이미 같은 슬롯에 장비가 있다면 먼저 해제
+        //if (currentEquipments.ContainsKey(newItem.slotType))
+        //{
+        //    Unequip(newItem.slotType);
+        //}
+
+        //// 2. 데이터 등록
+        //currentEquipments[newItem.slotType] = newItem;
+
+        //// 3. 능력치 적용 (PlayerStats 이용)
+        //stats.EquipItemModifiers(newItem.modifiers, newItem);
+
+        //// 4. 외형 업데이트
+        //visuals.UpdateVisual(newItem.slotType, newItem.equipmentSprite);
+
+        //// 5. 무기일 경우 무기 컨트롤러에 알림
+        //if (newItem is WeaponItem weaponItem)
+        //{
+        //    weaponController.EquipWeapon(weaponItem);
+        //}
+
+        //Log.Game($"{newItem.itemName} 장착 완료");
+    }
+
+    public void Equip(EquipmentSlot slot, int subIndex)
+    {
+        // 혹시 모를 NullReference 방지 위해 TryGetValue 사용
+        if (!currentEquipments.TryGetValue(slot, out var item))
+            return;
+
         // 1. 이미 같은 슬롯에 장비가 있다면 먼저 해제
-        if (currentEquipments.ContainsKey(newItem.slotType))
-        {
-            Unequip(newItem.slotType);
-        }
+        // currentDictionary의 경우 기본 값으로 초기화가 되어있기 때문에
+        // itemId가 0이 아닌 경우 장착된 아이템이 존재한다고 판단할 수 있다.
+        if (item[subIndex].itemId != 0)
+            Unequip(slot, subIndex);
 
         // 2. 데이터 등록
-        currentEquipments[newItem.slotType] = newItem;
+        currentEquipments[slot][subIndex] = item[subIndex];
+        EquipmentItem newItem = ItemDatabase.Instance.GetItem(item[subIndex].itemId) as EquipmentItem;
 
         // 3. 능력치 적용 (PlayerStats 이용)
         stats.EquipItemModifiers(newItem.modifiers, newItem);
@@ -66,13 +134,21 @@ public class PlayerEquipment : MonoBehaviour
         Log.Game($"{newItem.itemName} 장착 완료");
     }
 
-    public void Unequip(EquipmentSlot slot)
+    // 장비 해제 로직 (장착된 아이템이 있을 때만 호출)
+    public void Unequip(EquipmentSlot slot, int subIndex)
     {
-        if (!currentEquipments.TryGetValue(slot, out EquipmentItem item))
+        // 혹시 모를 NullReference 방지 위해 TryGetValue 사용
+        if (!currentEquipments.TryGetValue(slot, out var item))
             return;
 
+        // itemId가 0이라면 해당 슬롯에 장착된 아이템이 없다는 뜻이므로 해제할 필요가 없음
+        if (item[subIndex].itemId == 0)
+            return;
+
+        EquipmentItem equipmentItem = ItemDatabase.Instance.GetItem(item[subIndex].itemId) as EquipmentItem;
+
         // 1. 능력치 제거
-        stats.UnequipItemModifiers(item);
+        stats.UnequipItemModifiers(equipmentItem);
 
         // 2. 외형 제거
         visuals.ClearVisual(slot);
@@ -84,8 +160,8 @@ public class PlayerEquipment : MonoBehaviour
         }
 
         // 4. 데이터 제거
-        currentEquipments.Remove(slot);
+        currentEquipments[slot][subIndex].Clear();
 
-        Log.Game($"{item.itemName} 해제 완료");
+        Log.Game($"{equipmentItem.itemName} 해제 완료");
     }
 }
